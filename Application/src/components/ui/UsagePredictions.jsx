28ac4dfd263046620b0f28fb9b6869a4247b63cf
Paddo0 <paddo2003@gmail.com>
2024-08-23 11:15:55 +1000

    Initial statistics pages implementation.

---
+++ Application/src/components/ui/UsagePredictions.jsx
@@ -0,0 +1,16 @@
+
+
+/**
+ * Defines the prediction display component to display predicted usage
+ * @returns {React.JSX.Element} Component containing prediction component
+ */
+function UsagePredictions()
+{
+    return(
+        <div className="UsagePredictions">
+            Usage Predictions
+        </div>
+    )
+}
+
+export default UsagePredictions;
