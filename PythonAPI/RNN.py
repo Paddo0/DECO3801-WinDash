e822041a90786b5145dccce82c04a933c46eb9ce
Nicholas Chay <nichay9787@gmail.com>
2024-08-30 09:24:59 +1000

    updated architecture from vanilla RNN to LSTM

--- PythonAPI/RNN.py
+++ PythonAPI/RNN.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
+from torch.autograd import Variable
 
 
 #training data import 
@@ -17,74 +18,81 @@ df['Voltage'] = df['Voltage'].astype(float)
 
 scaler = MinMaxScaler(feature_range=(-1, 1))
 df['Voltage'] = scaler.fit_transform(df['Voltage'].values.reshape(-1,1))
+V_ten = torch.FloatTensor(df['Voltage']).view(-1)
+
 
 # TODO Prepare the input and target sequences
-def create_sequences(data, slen):
-    sequences = []
-    targets = []
-    for i in range(len(data) - slen):
-        seq = data[i:i + slen]
-        target = data[i + slen]
-        sequences.append(seq)
-        targets.append(target)
-    return torch.FloatTensor(sequences), torch.FloatTensor(targets)
+# Define the look-back period (e.g., 60 time steps)
+look_back = 60
 
-class RNNModel(nn.Module):
+# Create input-output pairs
+def create_inout_sequences(input_data, tw):
+    inout_seq = []
+    L = len(input_data)
+    for i in range(L-tw):
+        train_seq = input_data[i:i+tw]
+        train_label = input_data[i+tw:i+tw+1]
+        inout_seq.append((train_seq ,train_label))
+    return inout_seq
+
+train_sequences = create_inout_sequences(V_ten, look_back)
+
+class LSTM(nn.Module):
     # TODO Create main RNN class for model initialization 
-    def __init__(self, input_size, hidden_size, num_layers, output_size):
-        super(RNNModel, self).__init__()
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-    
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        
-        
-        self.fc = nn.Linear(hidden_size, output_size)
+    def __init__(self, input_size, hidden_size, output_size):
+        super(LSTM, self).__init__()
+        self.hidden_layer_size = hidden_size
+        self.lstm = nn.LSTM(input_size, hidden_size)
+        self.linear = nn.Linear(hidden_size, output_size)
+        self.hidden_cell = (torch.zeros(1, 1, self.hidden_layer_size),
+                            torch.zeros(1, 1, self.hidden_layer_size))
 
 
-def forward(self, x):
-    h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).requires_grad_()
-    c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).requires_grad_()
-
-    # Forward propagation
-    out, _ = self.lstm(x, (h0.detach(), c0.detach()))
-
-    # Vary output depending on how many predictions we need 
-    out = self.fc(out[:, -1, :])
-    return out
+def forward(self, input_seq):
+    lstm_out, self.hidden_cell = self.lstm(input_seq.view(len(input_seq) ,1, -1), self.hidden_cell)
+    predictions = self.linear(lstm_out.view(len(input_seq), -1))
+    return predictions[-1]
 
 
 # TODO Tune hyperparameters 
 
-input_size = 0
-hidden_size = 0
-output_size = 0
-num_layers = 0
+input_size = 1
+hidden_size = 100
+output_size = 1
+
 
 
 # Model initialization 
-#model = RNNModel(input_size, hidden_size, output_size, num_layers)
+model = LSTM(input_size, hidden_size, output_size)
 
 # TODO Selct appropriate loss criterion and optimizer 
-criterion = None
-optimizer = None
+criterion = torch.optim.AdamW()
+optimizer = nn.MSELoss()
 
 # TODO determine sufficient number of epochs 
-num_epochs = None
+epochs = 10
 
 # Training Loop 
-def train(model, criterion, optimizer):
-    for epoch in range(num_epochs):
-        model.train()
-        outputs = model()
-        optimizer.zero_grad()
-        loss = criterion(outputs, )
-        loss.backward()
-        optimizer.step()
-        
-        if epoch % 10 == 0:
-            print(f'Epoch [{epoch}/{num_epochs}], Loss: {loss.item():.2f}')
 
+def train(epochs):
+    for i in range(epochs):
+        for seq, labels in train_sequences:
+            optimizer.zero_grad()
+            model.hidden_cell = (torch.zeros(1, 1, model.hidden_layer_size),
+                            torch.zeros(1, 1, model.hidden_layer_size))
+
+            y_pred = model(seq)
+
+            single_loss = criterion(y_pred, labels)
+            single_loss.backward()
+            optimizer.step()
+
+        if i % 25 == 1:
+            print(f'Epoch {i} loss: {single_loss.item()}')
+
+    print(f'Epoch {i} loss: {single_loss.item()}')
+
+    
 def get_pred():
     # currently a vector of zeros as test 
     return [0,0,0,0,0]
