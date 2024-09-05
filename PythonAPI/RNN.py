c8742baad8aa3a073fe958ba4b36e284c177a675
Nicholas Chay <nichay9787@gmail.com>
2024-09-06 08:14:40 +1000

    Added saving and loading of model weights

--- PythonAPI/RNN.py
+++ PythonAPI/RNN.py
@@ -6,31 +6,34 @@ from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 from torch.autograd import Variable
 
+#Check for GPU availability
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 #training data import 
 df = pd.read_csv('PythonAPI\\data\\household_power_consumption.txt', sep=';')
 #print(df["Voltage"])
 
 # Normalization, using mean 0 var 1
 
-df['Voltage'] = pd.to_numeric(df['Voltage'], errors='coerce')
-df['Voltage'] = df['Voltage'].astype(float)
+df['Global_intensity'] = pd.to_numeric(df['Global_intensity'], errors='coerce')
+df['Global_intensity'] = df['Global_intensity'].astype(float)
 
 scaler = MinMaxScaler(feature_range=(-1, 1))
-values = df['Voltage'][:2000]
-df['Voltage'] = scaler.fit_transform(df['Voltage'].values.reshape(-1,1))
-V_ten = torch.FloatTensor(df['Voltage'][:2000]).view(-1)
+values = df['Global_intensity'][:10000]
+df['Global_intensity'] = scaler.fit_transform(df['Global_intensity'].values.reshape(-1,1))
+V_ten = torch.FloatTensor(df['Global_intensity'][:10000]).view(-1).to(device)
 
 
 # TODO Prepare the input and target sequences
-# Define the look-back period (e.g., 60 time steps)
-look_back = 120
+# look-back period
+look_back = 240
 
-# Create input-output pairs
 def create_inout_sequences(input_data, tw):
+    "Creates input and output sequences out of a Tensor"
     inout_seq = []
     L = len(input_data)
     for i in range(L-tw):
-        train_seq = input_data[i:i+tw]
+        train_seq = input_data[i:i+tw] 
         train_label = input_data[i+tw:i+tw+1]
         inout_seq.append((train_seq ,train_label))
     return inout_seq
@@ -42,15 +45,15 @@ class LSTM(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(LSTM, self).__init__()
         self.hidden_layer_size = hidden_size
-        self.lstm = nn.LSTM(input_size, hidden_size)
-        self.linear = nn.Linear(hidden_size, output_size)
-        self.hidden_cell = (torch.zeros(2, 1, self.hidden_layer_size),
-                            torch.zeros(2, 1, self.hidden_layer_size))
+        self.lstm = nn.LSTM(input_size, hidden_size).to(device)
+        self.linear = nn.Linear(hidden_size, output_size).to(device)
+        self.hidden_cell = (torch.zeros(2, 1, self.hidden_layer_size).to(device),
+                            torch.zeros(2, 1, self.hidden_layer_size).to(device))
 
 
     def forward(self, input_seq):
-        lstm_out, self.hidden_cell = self.lstm(input_seq.view(len(input_seq) ,1, -1), self.hidden_cell)
-        predictions = self.linear(lstm_out.view(len(input_seq), -1))
+        lstm_out, self.hidden_cell = self.lstm(input_seq.view(len(input_seq) ,1, -1).to(device), self.hidden_cell)
+        predictions = self.linear(lstm_out.view(len(input_seq), -1).to(device))
         return predictions[-1]
 
 
@@ -63,10 +66,10 @@ output_size = 1
 
 
 # Model initialization 
-model = LSTM(input_size, hidden_size, output_size)
+model = LSTM(input_size, hidden_size, output_size).to(device)
 
 # TODO Selct appropriate loss criterion and optimizer 
-criterion = nn.MSELoss()
+criterion = nn.MSELoss().to(device)
 optimizer = torch.optim.AdamW(model.parameters(), lr=0.001)
 
 # TODO determine sufficient number of epochs 
@@ -77,9 +80,10 @@ epochs = 20
 def train(epochs):
     for i in range(epochs):
         for seq, labels in train_sequences:
+            seq, labels = seq.to(device), labels.to(device)
             optimizer.zero_grad()
-            model.hidden_cell = (torch.zeros(1, 1, model.hidden_layer_size),
-                            torch.zeros(1, 1, model.hidden_layer_size))
+            model.hidden_cell = (torch.zeros(1, 1, model.hidden_layer_size).to(device),
+                            torch.zeros(1, 1, model.hidden_layer_size).to(device))
 
             y_pred = model(seq)
 
@@ -87,20 +91,25 @@ def train(epochs):
             single_loss.backward()
             optimizer.step()
 
-        if i % 1 == 1:
+        if i % 1 == 0:
             print(f'Epoch {i} loss: {single_loss.item()}')
 
+    torch.save(model.state_dict(), 'model_weights.pth')
+
     print(f'Epoch {i} loss: {single_loss.item()}')
+
+def test():
+    model.load_state_dict(torch.load('model_weights.pth'))
 
     model.eval()
 
     test_inputs = V_ten[-look_back:].tolist()
 
     for i in range(look_back):
-        seq = torch.FloatTensor(test_inputs[-look_back:])
+        seq = torch.FloatTensor(test_inputs[-look_back:].to(device))
         with torch.no_grad():
-            model.hidden_cell = (torch.zeros(1, 1, model.hidden_layer_size),
-                                torch.zeros(1, 1, model.hidden_layer_size))
+            model.hidden_cell = (torch.zeros(1, 1, model.hidden_layer_size).to(device),
+                                torch.zeros(1, 1, model.hidden_layer_size).to(device))
             test_inputs.append(model(seq).item())
 
     # Inverse transform the predicted values to original scale
@@ -113,6 +122,7 @@ def train(epochs):
     plt.plot(np.arange(len(values), len(values) + look_back), predicted_values, label='Predictions')
     plt.legend()
     plt.show()
+    plt.savefig('prediction.png')
 
     
 def get_pred():
@@ -120,3 +130,4 @@ def get_pred():
     return [0,0,0,0,0]
 
 train(epochs)
+test()
