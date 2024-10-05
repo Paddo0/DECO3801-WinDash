e52f21a1ad0d4c76bc65c2456495266175eb097d
Paddo0 <107907086+Paddo0@users.noreply.github.com>
2024-10-05 19:30:58 +1000

    Implemented timer and fixed residual bugs. (#47)

--- Application/src/index.jsx
+++ Application/src/index.jsx
@@ -6,7 +6,5 @@ import App from './App';
 // Defining root object as App
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
